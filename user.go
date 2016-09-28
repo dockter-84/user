f2e30101f53e97890a70470f4ad61ef2d940361a
Michael Dockter <dockter-84@users.noreply.github.com>
2016-09-28 10:41:11 -0400

    Create user.go

---
+++ user.go
@@ -0,0 +1,14 @@
+// This package implements the "bluemix-spark-utils system-check disk-full" subcommand.
+package unixuser
+
+import (
+  "os/user"
+)
+
+func Lookup(username string) (*user.User, error) {
+  return nil, nil
+}
+
+func LookupGroup(name string) (*user.Group, error) {
+  return nil, nil
+}
